891a67618877e66ff003292ed5d8ab611d7edfb1
BKVDPrasad <54040086+BKVDPrasad@users.noreply.github.com>
2020-08-06 22:03:20 +0530

    Update settings.py

--- bigP/settings.py
+++ bigP/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't6f!#+d4+@ix%%pf-exhcndps12g!v(umygda2jl-p#!g7l0ky'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
