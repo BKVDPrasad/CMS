03abbc499e28245db661c53f6172e653040b1637
BKVDPrasad <54040086+BKVDPrasad@users.noreply.github.com>
2024-04-15 19:11:38 +0530

    Update settings.py

--- bigP/settings.py
+++ bigP/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
+    'app3',
+    'rest_framework',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
